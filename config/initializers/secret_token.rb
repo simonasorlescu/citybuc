6b781dc5fd636bb07126cca48daa509132debaf1
simonasorlescu <simona.sorlescu@gmail.com>
2013-04-25 10:49:34 +0300

    Create cityguide app with MySQL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cityguide::Application.config.secret_token = '9dd6cbf21ae3540783adeb7470b26f744dff3f764a9eb8dd1c481f9d9d51b2f614c10301e905a205174f8d107e058f45bd16fb5f95391de7c1ee6d7f58c52a82'
